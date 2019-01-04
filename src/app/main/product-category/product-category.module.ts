221f88dc0c9718a3f4fac425145843cb6a640f18
nhirambo <nhirambo@gmail.com>
2019-01-04 22:46:45 +0700

    2019/01/04 lession10:building structure for application

---
+++ src/app/main/product-category/product-category.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCategoryComponent } from './product-category.component';
+
+@NgModule({
+  declarations: [ProductCategoryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductCategoryModule { }
